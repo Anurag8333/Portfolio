06763aa28f941e8eda4e167046e8537f6be464c9
Anurag8333 <163815550+Anurag8333@users.noreply.github.com>
2024-07-11 16:34:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function typing(){
+    var typed = new Typed(".textchange",{
+        strings:["Web Developer","Coder","Java Developer","Software Engineer"],
+        typeSpeed:100,
+        backSpeed:100,
+        loop :true
+    })
+}
+typing();
